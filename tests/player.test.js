e38dcac55475da29b059378297f870f2c1ce9fe2
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-06-15 19:54:24 +0200

    test: implement tests for gameboard getter and isComputer method

---
+++ tests/player.test.js
@@ -0,0 +1,13 @@
+import Gameboard from '../src/js/gameboard';
+import Player from '../src/js/player';
+
+let player = new Player('person');
+let computer = new Player('computer');
+
+test('Player type is a computer.', () => {
+  expect(computer.isComputer()).toBe(true);
+});
+
+test('Player gameboard is instance of Gameboard class.', () => {
+  expect(player.gameboard).toBeInstanceOf(Gameboard);
+});
