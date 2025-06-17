553a99afab942ed6d953fe82857532bfa0ad2f96
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-06-17 09:22:14 +0200

    feat: rename div to row for easier css and eventlistener targeting

--- src/js/ui/gameboardUI.js
+++ src/js/ui/gameboardUI.js
@@ -1,17 +1,19 @@
 export default class GameboardUI {
   static generate(player) {
     const type = player.isComputer() ? 'computer' : 'person';
-    const container = document.getElementById(`${type}Board`);
+    const gameboard = document.getElementById(`${type}Board`);
 
     for (let i = 0; i < 10; i++) {
-      const divContainer = document.createElement('div');
-      divContainer.id = `row-${i}`;
+      const row = document.createElement('div');
+      row.id = `row-${i}`;
+
       for (let j = 0; j < 10; j++) {
         const div = document.createElement('div');
         div.id = `${i},${j}`;
-        divContainer.appendChild(div);
+        row.appendChild(div);
       }
-      container.appendChild(divContainer);
+
+      gameboard.appendChild(row);
     }
   }
 }
