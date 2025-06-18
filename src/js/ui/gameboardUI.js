27fea80362c5d90c5442b6f9e55bc1fbc6df81bc
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-06-18 08:56:53 +0200

    fix: disable ship placement if no valid coordinates/coordinates taken

--- src/js/ui/gameboardUI.js
+++ src/js/ui/gameboardUI.js
@@ -1,3 +1,5 @@
+import UI from './ui';
+
 export default class GameboardUI {
   static generate(player) {
     const type = player.isComputer() ? 'computer' : 'person';
@@ -24,18 +26,20 @@ export default class GameboardUI {
     let shipLength = player.gameboard.nextShipSize();
 
     gameboard.addEventListener('mouseup', (event) => {
-      const coords = this.getCoordsFromEvent(event, player, shipLength);
-      this.highlightCell(coords, gameboard, 'ship');
+      const coords = UI.getCoordsFromEvent(event, player, shipLength);
+      UI.highlightCell(coords, gameboard, 'ship');
 
-      player.gameboard.placeShip(coords);
-      shipLength = player.gameboard.nextShipSize();
+      if (coords) {
+        player.gameboard.placeShip(coords);
+        shipLength = player.gameboard.nextShipSize();
 
-      if (!shipLength) {
-        gameboard.classList.add('disabled');
-        gameboard.style.pointerEvents = 'none';
+        if (!shipLength) {
+          gameboard.classList.add('disabled');
+          gameboard.style.pointerEvents = 'none';
 
-        console.log('All ships placed! Ready to start game.');
-        // Call next game phase here (for tomorrow!!)
+          console.log('All ships placed! Ready to start game.');
+          // Call next game phase here (for tomorrow!!)
+        }
       }
     });
 
@@ -47,40 +51,8 @@ export default class GameboardUI {
         .querySelectorAll('.cell.preview')
         .forEach((cell) => cell.classList.remove('preview'));
 
-      const coords = this.getCoordsFromEvent(event, player, shipLength);
-      this.highlightCell(coords, gameboard, 'preview');
+      const coords = UI.getCoordsFromEvent(event, player, shipLength);
+      UI.highlightCell(coords, gameboard, 'preview');
     });
-  }
-
-  static generateCoordinates(player, [x, y], length, direction = 'horizontal') {
-    const coordinates = [];
-    for (let i = 0; i < length; i++) {
-      const position = direction === 'horizontal' ? [x + i, y] : [x, y + 1];
-      if (
-        !player.gameboard.validateCoordinates(position) ||
-        player.gameboard.hasShipAt(position.join(','))
-      )
-        return null;
-      coordinates.push(position);
-    }
-    return coordinates;
-  }
-
-  static highlightCell(coords, gameboard, className) {
-    if (!coords) return;
-    for (const [cx, cy] of coords) {
-      const cell = gameboard.querySelector(
-        `.cell[data-x="${cx}"][data-y="${cy}"]`
-      );
-      if (cell) cell.classList.add(className);
-    }
-  }
-
-  static getCoordsFromEvent(event, player, shipLength) {
-    const x = parseInt(event.target.dataset.x);
-    const y = parseInt(event.target.dataset.y);
-    if (isNaN(x) || isNaN(y)) return;
-
-    return this.generateCoordinates(player, [x, y], shipLength);
   }
 }
