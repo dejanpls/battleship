6fc8b954fda74da841f73cf7a0195634332199d9
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-06-18 08:56:00 +0200

    refactor: move helper methods to ui.js module

---
+++ src/js/ui/ui.js
@@ -0,0 +1,33 @@
+export default class UI {
+  static generateCoordinates(player, [x, y], length, direction = 'horizontal') {
+    const coordinates = [];
+    for (let i = 0; i < length; i++) {
+      const position = direction === 'horizontal' ? [x + i, y] : [x, y + 1];
+      if (
+        !player.gameboard.validateCoordinates(position) ||
+        player.gameboard.hasShipAt(position.join(','))
+      )
+        return null;
+      coordinates.push(position);
+    }
+    return coordinates;
+  }
+
+  static highlightCell(coords, gameboard, className) {
+    if (!coords) return;
+    for (const [cx, cy] of coords) {
+      const cell = gameboard.querySelector(
+        `.cell[data-x="${cx}"][data-y="${cy}"]`
+      );
+      if (cell) cell.classList.add(className);
+    }
+  }
+
+  static getCoordsFromEvent(event, player, shipLength) {
+    const x = parseInt(event.target.dataset.x);
+    const y = parseInt(event.target.dataset.y);
+    if (isNaN(x) || isNaN(y)) return;
+
+    return this.generateCoordinates(player, [x, y], shipLength);
+  }
+}
