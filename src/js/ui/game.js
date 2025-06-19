25522299d32de348639a9cfa789712346d5ee878
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-06-19 11:19:40 +0200

    feat: enahance Game class with start method which initializes the game

---
+++ src/js/ui/game.js
@@ -0,0 +1,14 @@
+import Player from '../core/player';
+import GameboardUI from './gameboardUI';
+import UI from './ui';
+
+export default class Game {
+  static start() {
+    const person = new Player('person');
+    const computer = new Player('computer');
+    GameboardUI.generate(person);
+    GameboardUI.generate(computer);
+    GameboardUI.placeShip(person, computer);
+    UI.toggleNotifications();
+  }
+}
